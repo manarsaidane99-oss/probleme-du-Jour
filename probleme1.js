4cc340ac4701fc3983a96a0da87b8998cb5e2d19
manar <manarsaidane99@gmail.com>
2026-08-01 08:48:00 +0100

    ee

--- probleme1.js
+++ probleme1.js
@@ -1,6 +1,9 @@
 // 1-Créer une fonction qui combine ton prénom et ton nom et retourne le résultat avec un espace entre les deux.
 
-
+function combine(nom,prenom) {
+  return nom+" "+prenom
+}
+console.log(combine("manar","saidane"));
 
 
 
@@ -10,7 +13,11 @@
 
 // 2-Écris une fonction qui prend deux nombres et retourne la moyenne.
 
-
+function moyenne(num1,num2) 
+{
+  return (num1+num2)/2
+}
+console.log(moyenne(2,5) );
 
 
 
@@ -20,7 +27,11 @@
 
 // 3-Crée une fonction qui prend une chaîne de caractères et retourne le nombre de caractères qu'elle contient.
 
+function nomberChar(string){
+ return string.length
 
+}
+console.log(nomberChar("manar"));
 
 
 
@@ -30,7 +41,10 @@
 
 // 4-Crée une fonction qui retourne la sous-chaîne à partir d'une position donnée jusqu'à la fin.
 
-
+function sousCHan(string1,position){
+ return string1.slice(position)
+}
+console.log(sousCHan("manar",1));
 
 
 
